d2e78071dbac27aefc0cae040b49d2ac120080b0
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-06-30 18:04:04 +0530

    [Sairam]Added:ability update  the table of the database

--- payroll/pay.sql
+++ payroll/pay.sql
@@ -12,3 +12,8 @@ values('William', 100000, convert (date,'2010-8-19'));
 select * from EmpPayroll;
 select Salary from EmpPayroll where EmployeeName='William';
 select * from EmpPayroll where StartingDate between cast('2005-8-19' as date) and cast('2015-8-19' as date); 
+
+alter table EmpPayroll
+add Gender varchar(255);
+update EmpPayroll set Gender='Female' where EmployeeName = 'Smithi' or EmployeeName='Mandana';
+update EmpPayroll set Gender='Male'where EmployeeName='William';
