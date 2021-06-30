08bdd9e2f722eb244a16d95fe3af0369759e93d6
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-06-30 17:54:15 +0530

    [Sairam]Added:ability to retrive particular data in the table of the database

--- payroll/pay.sql
+++ payroll/pay.sql
@@ -10,3 +10,5 @@ insert into EmpPayroll(EmployeeName, Salary, StartingDate)
 values('William', 100000, convert (date,'2010-8-19'));
 
 select * from EmpPayroll;
+select Salary from EmpPayroll where EmployeeName='William';
+select * from EmpPayroll where StartingDate between cast('2005-8-19' as date) and cast('2015-8-19' as date); 
