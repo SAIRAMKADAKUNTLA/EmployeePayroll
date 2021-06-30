52dd3e2f686425aa5f8831375d0d5385f8ba9913
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-06-30 17:34:45 +0530

    [Sairam]Added:ability to create a table in database

--- payroll/pay.sql
+++ payroll/pay.sql
@@ -1,2 +1,8 @@
 create database PayrollService;
 use PayrollService;
+create table EmpPayroll(
+EmployeeId int identity(1,1) primary key,
+EmployeeName varchar(255),
+Salary float,
+StartingDate Date
+);
