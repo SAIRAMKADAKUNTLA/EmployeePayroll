8aa34eec68f6010833bbacf10417866616b75bc2
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-06-30 17:41:48 +0530

    [Sairam]Added:ability to insert data in the table of the database

--- payroll/pay.sql
+++ payroll/pay.sql
@@ -6,3 +6,6 @@ EmployeeName varchar(255),
 Salary float,
 StartingDate Date
 );
+insert into EmpPayroll(EmployeeName, Salary, StartingDate)
+values('William', 100000, convert (date,'2010-8-19'));
+select * from EmpPayroll;
